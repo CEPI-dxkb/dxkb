ce9362f4dc30df3762c7532ab74208ada55d3004
Chris Escobar <chrescobar@uchicago.edu>
2026-02-17 15:56:13 -0600

    Update <Collapsible /> component to use base-ui, add open and close annimation as well

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,36 +1,27 @@
 "use client"
 
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
-import { Separator } from "./separator"
+import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible"
 
-function Collapsible({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+function Collapsible({ ...props }: CollapsiblePrimitive.Root.Props) {
   return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
 }
 
-function CollapsibleTrigger({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+function CollapsibleTrigger({ ...props }: CollapsiblePrimitive.Trigger.Props) {
   return (
-    <CollapsiblePrimitive.CollapsibleTrigger
-      data-slot="collapsible-trigger"
-      {...props}
-    />
+    <CollapsiblePrimitive.Trigger data-slot="collapsible-trigger" {...props} />
   )
 }
 
 function CollapsibleContent({
+  keepMounted = true,
   ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+}: CollapsiblePrimitive.Panel.Props) {
   return (
-    <CollapsiblePrimitive.CollapsibleContent
+    <CollapsiblePrimitive.Panel
       data-slot="collapsible-content"
+      keepMounted={keepMounted}
       {...props}
-    >
-      <Separator />
-      {props.children}
-    </CollapsiblePrimitive.CollapsibleContent>
+    />
   )
 }
 
